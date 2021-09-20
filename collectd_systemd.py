fc014a1a0bb691fca917bb6517846d9ec20c1a5e
Nikos Tsipinakis <nikos.tsipinakis@cern.ch>
2021-09-20 13:57:31 +0200

    Log service type

--- collectd_systemd.py
+++ collectd_systemd.py
@@ -90,9 +90,9 @@ class SystemD(object):
                 self.init_dbus()
                 state = self.get_service_state(full_name)
 
-            value = (1.0 if state == 'running' or state == 'reload' or state == 'start' or ( state == 'dead' and type == 'oneshot' )  else 0.0)
-            self.log_verbose('Sending value: {}.{}={} (state={})'
-                             .format(self.plugin_name, name, value, state))
+            value = (1.0 if state == 'running' or state == 'reload' or state == 'start' or ( state == 'dead' and type == 'oneshot' ) else 0.0)
+            self.log_verbose('Sending value: {}.{}={} (state={}, type={})'
+                             .format(self.plugin_name, name, value, state, type))
             val = collectd.Values(
                 type='gauge',
                 plugin=self.plugin_name,
